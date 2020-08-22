c1ab693854d4c87e2770abaf3075a851177db35b
Mash <40543693+mashaole@users.noreply.github.com>
2020-08-22 21:16:28 +0200

    Updates

---
+++ precache-manifest.525a7e20e28f4230537de49cc14794a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a6c87c544fa77b9a96ea5df54cd0ae",
+    "url": "/LAPA/index.html"
+  },
+  {
+    "revision": "caedcd34ea102e854485",
+    "url": "/LAPA/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6fc0d8cc8faf62d7601a",
+    "url": "/LAPA/static/js/2.52fcb4f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LAPA/static/js/2.52fcb4f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caedcd34ea102e854485",
+    "url": "/LAPA/static/js/main.7f78c93b.chunk.js"
+  },
+  {
+    "revision": "86a1d819414f177c8ef0",
+    "url": "/LAPA/static/js/runtime-main.d512f004.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/LAPA/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lapa/precache-manifest.f474ed17845b7813eb63ab62ea5053a1.js"
+  "/LAPA/precache-manifest.525a7e20e28f4230537de49cc14794a3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lapa/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/LAPA/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
