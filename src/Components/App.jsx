d3c8ee589807ebc153996baad795b05eeba3064b
Mash <40543693+mashaole@users.noreply.github.com>
2020-08-23 17:31:42 +0200

    -Contact US section

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -3,20 +3,28 @@ import '../App.css'
 import logo from './assets/loader.png';
 import Navbar from './Nav/Navbar';
 import Footer from './Nav/Footer';
+import ContactForm from './Form/ContactForm';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <body className="App-header">
-        <h1>
-          Coming Soon
+      <div>
+        <body className="App-header">
+          <h1>
+            Coming Soon
         </h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          LAPA eCommerce site
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            LAPA eCommerce site
       </p>
-      </body>
+        </body>
+      </div>
+      <div>
+        <body className="App-header">
+          <ContactForm />
+        </body>
+      </div>
       <Footer />
     </div>
   );
---
+++ src/Components/Form/ContactForm.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+class ContactForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            phone: '',
+            address: '',
+            message: ''
+        };
+
+        this.handleChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+        console.log('Change detected. State updated' + name + ' = ' + value);
+    }
+
+    handleSubmit(event) {
+        alert('A form was submitted: ' + this.state.name + ' // ' + this.state.email);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="contact">
+                <h1>Contact Us</h1>
+                <form onSubmit={this.handleSubmit} >
+                    <div className="form-group">
+                        <input type="text" name="name" value={this.state.name} onChange={this.handleChange} className="form-control" id="nameImput" placeholder="Full Name" />
+                    </div>
+                    <div className="form-group">
+                        <input name="email" type="email" value={this.state.email} onChange={this.handleChange} className="form-control" id="emailInput" placeholder="Email" />
+                    </div>
+                    <div className="form-group">
+                        <input name="phone" type="email" value={this.state.phone} onChange={this.handleChange} className="form-control" id="phoneInput" placeholder="Phone Number" />
+                    </div>
+                    <div className="form-group">
+                        <input name="address" type="email" value={this.state.address} onChange={this.handleChange} className="form-control" id="addressInput" placeholder="Address" />
+                    </div>
+                    <div className="form-group">
+                        <textarea name="message" type="textarea" rows="4" cols="22" value={this.state.message} onChange={this.handleChange} className="form-control" id="messageImput" placeholder="Message" />
+                    </div>
+                    <input type="submit" value="Submit" className="btn btn-primary" />
+                </form>
+            </div>
+        )
+    }
+}
+export default ContactForm;
