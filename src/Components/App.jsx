88dbdc431368b6abd4d81bdb4c633747aa11ea9f
Mash <40543693+mashaole@users.noreply.github.com>
2020-08-31 00:42:06 +0200

    About us component , page routers,

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -5,32 +5,61 @@ import Footer from './Nav/Footer';
 import loading from './assets/load.png';
 import ContactForm from './Form/ContactForm';
 import MasonryLayout from './Layout/MasoryLayout';
+import About from './Layout/About'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { func } from 'prop-types';
+
+function Nav() {
+  return (<Navbar />)
+}
+
+function Foot() {
+  return (<Footer />)
+}
+
+function Contact() {
+  return (<ContactForm />)
+}
+
+function Landing() {
+  return (<MasonryLayout />)
+}
+
+function ComingSoon() {
+  return (<div>
+    <h1>
+      Coming Soon
+        </h1>
+    <img src={loading} className="App-logo" alt="loading" />
+    <p>
+      LAPA eCommerce site
+      </p>
+  </div>)
+}
+
+function AboutUs() {
+  return (<About />)
+}
+
+function inBoundUser() {
+  return (<div className="App">
+    {Nav()}
+    <body className="App-header">
+      <Router>
+        <Route exact path="/">{Landing()} </Route>
+        <Route exact path="/products">{ComingSoon()}</Route>
+        <Route exact path="/about">{AboutUs()}</Route>
+        <Route exact path="/contact">{Contact()}</Route>
+      </Router>
+    </body>
+    <div>
+    </div>
+    {Foot()}
+  </div>)
+}
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div>
-        <body className="App-header">
-          <MasonryLayout />
-          <h1>
-            Coming Soon
-        </h1>
-          <img src={loading} className="App-logo" alt="loading" />
-          <p>
-            LAPA eCommerce site
-      </p>
-        </body>
-      </div>
-      <div>
-        <body className="App-header">
-          <ContactForm />
-        </body>
-      </div>
-      <Footer />
-    </div>
-  );
+  return (inBoundUser());
 }
 
 export default App;
---
+++ src/Components/Layout/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class About extends React.Component {
+    render() {
+        return (
+            <div id="contain">
+                <h1>About Us</h1>
+                <img id="img" src="https://lh3.googleusercontent.com/3aS5iolKjwR3EU_D6BZ1y6nj9o9SrTh0gLDug4Gh8MKlddPtpzB0yVCyPD-80Cm7EK_tmV8ugK1dzn3CxcxpBirMvVtMn6HEgw=s1200" id="img" />
+                <p id="p">
+                    <h2>My Story</h2>
+                    Tiisetso Sebola is the name. I originate from the Limpopo province in Polokwane but later moved to Cape Town in pursuit of sharpening my culinary skills as we all know that it is the culinary capital of South Africa.
+                I had to choose between cooking and architecture but really, cooking is way better than drawing houses. There is an art in it.&nbsp
+Being a chef has always and forever been something I always wanted to pursue because I am very passionate about food and cooking.My mother’s cooking made me fall in love with food; she is an amazing cook! I would watch her make breads and watch the technicality behind her cooking.</p>
+            </div>
+        )
+    }
+}
